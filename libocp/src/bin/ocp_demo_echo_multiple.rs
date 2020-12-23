b7f75538173024e4097ca9bb89bdbcbcac0b5600
Philipp Schuster <phip1611@gmail.com>
2020-12-23 23:59:47 +0100

    libocp: test binary to test if there is a bug when using neli often (looks good) - 1000 echo messages

---
+++ libocp/src/bin/ocp_demo_echo_multiple.rs
@@ -0,0 +1,14 @@
+//! Demo for multiple ECHO command.
+
+use libocp::ocp_core::Ocp;
+
+/// Demo for multiple ECHO command.
+fn main() {
+    let mut ocp = Ocp::connect().unwrap();
+
+    for _ in 0..1000 {
+        let res = ocp.ocp_echo("HELLO FROM RUST!!").unwrap();
+        assert_eq!("HELLO FROM RUST!!", res.msg().unwrap())
+    }
+
+}
