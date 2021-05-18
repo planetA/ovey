af17563f1a3dd2243c91bda769ca5aede45c3edc
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2021-05-19 00:45:14 +0200

    Fail cli in case of error

--- ovey_cli/src/main.rs
+++ ovey_cli/src/main.rs
@@ -6,6 +6,8 @@ use liboveyutil::types::Uuid;
 
 mod daemon;
 
+type MyResult = std::result::Result<(), String>;
+
 fn main() {
     // if args are invalid this function will exit the program
     let matches = assert_and_get_args();
@@ -15,21 +17,26 @@ fn main() {
    /* let ga = Ocp::connect(FAMILY_NAME, verbosity).unwrap();
     println!("Family id of {} is {}", FAMILY_NAME, ga.family_id());*/
 
-    if let Some(matches) = matches.subcommand_matches("new") {
-        action_create_new_device(verbosity, matches);
+    let res = if let Some(matches) = matches.subcommand_matches("new") {
+        action_create_new_device(verbosity, matches)
     } else if let Some(matches) = matches.subcommand_matches("delete") {
-        action_delete_device(verbosity, matches);
+        action_delete_device(verbosity, matches)
     } else if let Some(matches) = matches.subcommand_matches("echo") {
-        action_echo(verbosity, matches);
+        action_echo(verbosity, matches)
     }  else if let Some(matches) = matches.subcommand_matches("list") {
-        action_list(verbosity, matches);
+        action_list(verbosity, matches)
     } else {
         eprintln!("Usage: ovey -h");
-    }
+        Err("Unknown command".to_string())
+    };
 
+    if let Err(err) = res {
+        eprintln!("Failed with: {}", err);
+        std::process::exit(-1);
+    }
 }
 
-fn action_create_new_device(verbosity: u8, matches: &ArgMatches) {
+fn action_create_new_device(verbosity: u8, matches: &ArgMatches) -> MyResult {
     let new_device_name = matches.value_of("name").unwrap(); // unwrap because required
     let parent_device_name = matches.value_of("parent").unwrap();
     let guid_str = matches.value_of("guid").unwrap();
@@ -56,21 +63,24 @@ fn action_create_new_device(verbosity: u8, matches: &ArgMatches) {
                         println!("Ovey device was created successfully. Response from daemon:");
                         println!("{:#?}", dto);
                     }
+                    Ok(())
                 }
                 Err(err) => {
                     eprintln!("Ovey device was NOT CREATED SUCCESSFULLY. Error from Ovey daemon:");
                     eprintln!("{}", err);
+                    Err(err)
                 }
             }
 
         }
         Err(err) => {
             eprintln!("Cannot create device. Malformed input. {}", err);
+            Err(err)
         }
     }
 }
 
-fn action_delete_device(verbosity: u8, matches: &ArgMatches) {
+fn action_delete_device(verbosity: u8, matches: &ArgMatches) -> MyResult {
     let device_name = matches.value_of("name").unwrap();
     if verbosity > 0 {
         println!("sending request to delete device on local machine: name={}", device_name);
@@ -89,34 +99,41 @@ fn action_delete_device(verbosity: u8, matches: &ArgMatches) {
                         println!("Ovey device was deleted successfully. Response from daemon:");
                         println!("{:#?}", dto);
                     }
+                    Ok(())
                 }
                 Err(err) => {
                     eprintln!("Ovey device was NOT DELETED SUCCESSFULLY. Error from Ovey daemon:");
                     eprintln!("{}", err);
+                    Err(err)
                 }
             }
 
         }
         Err(err) => {
             eprintln!("Cannot delete device. Malformed input. {}", err);
+            Err(err)
         }
     }
 }
 
 /// Queries the daemon and returns information about all local Ovey devices.
-fn action_list(_verbosity: u8, _matches: &ArgMatches) {
+fn action_list(_verbosity: u8, _matches: &ArgMatches) -> MyResult {
     let res = forward_list_to_daemon();
     match res {
         Ok(data) => {
             println!("Found the following Ovey devices:");
             // TODO make this pretty
             println!("{:#?}", data);
+            Ok(())
         }
-        Err(e) => {eprintln!("Cannot list devices. Couldn't fetch data from Ovey daemon ({}).", e)}
+        Err(e) => {
+            eprintln!("Cannot list devices. Couldn't fetch data from Ovey daemon ({}).", e);
+            Err(e)
+        }
     }
 }
 
-fn action_echo(verbosity: u8, matches: &ArgMatches) {
+fn action_echo(verbosity: u8, matches: &ArgMatches) -> MyResult {
     let value = matches.value_of("value").unwrap(); // unwrap
     if verbosity > 0 {
         println!("sending echo request with value={}", value);
@@ -125,5 +142,7 @@ fn action_echo(verbosity: u8, matches: &ArgMatches) {
     // TODO!!
     //let res = forward_echo_to_daemon()
     //println!("Received from kernel: {}", res.get_msg().unwrap());*/
+
+    Ok(())
 }
 
