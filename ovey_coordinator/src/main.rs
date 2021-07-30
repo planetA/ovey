52cc97c7b265991d6c061ae0e934f7e89eb384cd
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2021-07-30 13:38:32 +0200

    Remove unused test

--- ovey_coordinator/src/main.rs
+++ ovey_coordinator/src/main.rs
@@ -40,39 +40,3 @@ async fn main() -> std::io::Result<()> {
         .run()
         .await
 }
-
-#[cfg(test)]
-mod tests {
-    /*use super::*;
-    use actix_web::dev::Service;
-    use actix_web::{http, test, web, App};
-    use actix_web::http::Error;*/
-
-    /*#[actix_rt::test]
-    async fn test_index() -> Result<(), Error> {
-        let mut app = test::init_service(
-            App::new().service(web::resource("/").route(web::post().to(route_index))),
-        ).await;
-
-        let req = test::TestRequest::post()
-            .uri("/")
-            .set_json(&MyObj {
-                name: "my-name".to_owned(),
-                number: 43,
-                uuid: None
-            })
-            .to_request();
-        let resp = app.call(req).await.unwrap();
-
-        assert_eq!(resp.status(), http::StatusCode::OK);
-
-        let response_body = match resp.response().body().as_ref() {
-            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
-            _ => panic!("Response error"),
-        };
-
-        assert_eq!(response_body, r##"{"name":"my-name","number":43,"uuid":null}"##);
-
-        Ok(())
-    }*/
-}
